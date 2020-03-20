fddf9668fb25a2a520dfcd9f91a0adbe2951f8cd
evcou16 <62434178+evcou16@users.noreply.github.com>
2020-03-20 15:59:46 -0400

    définir initialisation des parties

--- api.py
+++ api.py
@@ -14,3 +14,15 @@ def lister_parties(idul):
         print("Le GET sur '{}' a produit le code d'erreur {}.".format(
             url_lister, response.status_code)
         )
+def initialiser_partie(idul):
+    url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
+    response = requests.post(url_lister, data={'idul': idul})
+    if response.status_code == 200:
+        response = response.text
+        json_var = json.loads(response)
+        json_str = json.dumps(json_var, indent=2)
+        print(json_str)
+    else:
+        print("Le POST sur '{}' a produit le code d'erreur {}.".format(
+            url_lister, response.status_code)
+        )
