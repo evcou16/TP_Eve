4bab634cdfe83b144884c46939cf70eeb5dd8526
evcou16 <62434178+evcou16@users.noreply.github.com>
2020-03-20 17:33:55 -0400

    changement initialiser partie

--- api.py
+++ api.py
@@ -16,15 +16,16 @@ def lister_parties(idul):
     except RuntimeError as erreur:
         print(erreur)
         
-def initialiser_partie(idul):
-    url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
-    response = requests.post(url_lister, data={'idul': idul})
-    if response.status_code == 200:
-        response = response.text
-        json_var = json.loads(response)
-        json_str = json.dumps(json_var, indent=2)
-        print(json_str)
-    else:
-        print("Le POST sur '{}' a produit le code d'erreur {}.".format(
-            url_lister, response.status_code)
-        )
+def initialiser_partie(idul):    
+    url_jouer= 'https://python.gel.ulaval.ca/quoridor/api/initialiser/'
+    try:
+        reponse = requests.post(url_jouer, data={'idul': idul})
+        if reponse.status_code == 200:
+            json_reponse = reponse.json()
+            return json_reponse['id'], json_reponse['état']
+        else:
+            print("Le POST sur '{}' a produit le code d'erreur {}".format(
+                url_jouer, reponse.status_code
+            ))
+    except RuntimeError as erreur:
+        print(erreur)
