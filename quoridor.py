fc9fed9e0d96e3d7037300f4817fb94579e47015
evcou16 <62434178+evcou16@users.noreply.github.com>
2020-03-20 11:21:41 -0400

    creation analyser commande

--- quoridor.py
+++ quoridor.py
@@ -1,1 +1,9 @@
+import argparse
 
+
+def analyser_commande():
+    parser = argparse.ArgumentParser(description="Jeu quoridor - Phase 1")
+    parser.add_argument("-l", "--lister", action="store_true",help="Lister les identifiants de vos 20 dernieres parties.")
+    parser.add_argument("idul", metavar="idul", type=str, help="IDUL du joueur")
+    #parser.add_argument("nom_du_joueur", metavar="nom_du_joueur", type=str, help="nom du joueur")
+    return parser.parse_args()
