0ddd56f21b01240668059bd5ef1fa4ac05e30daf
evcou16 <62434178+evcou16@users.noreply.github.com>
2020-03-20 11:28:46 -0400

    changement dans argparse

--- quoridor.py
+++ quoridor.py
@@ -5,7 +5,6 @@ def analyser_commande():
     parser = argparse.ArgumentParser(description="Jeu quoridor - Phase 1")
     parser.add_argument("-l", "--lister", action="store_true",help="Lister les identifiants de vos 20 dernieres parties.")
     parser.add_argument("idul", metavar="idul", type=str, help="IDUL du joueur")
-    #parser.add_argument("nom_du_joueur", metavar="nom_du_joueur", type=str, help="nom du joueur")
     return parser.parse_args()
 
 def ligne(i):
@@ -32,3 +31,5 @@ def afficher_damier_ascii(grille):
     espace_horizontal = ((board_positions * 4) - 1)
     jeu_position_x = range(1, (board_positions * 4), 4)
     jeu_position_y = range(((board_positions - 1) * 2), -1, -2)
+    legende = "legende: "
+    board = [legende]
