bc435d4f57bc66e759f7aa056266481bbc9cb50d
evcou16 <62434178+evcou16@users.noreply.github.com>
2020-03-20 17:24:57 -0400

    changement lister_parties

--- api.py
+++ api.py
@@ -4,16 +4,18 @@ import json
 
 def lister_parties(idul):
     url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
-    response = requests.get(url_lister, params={'idul': idul})
-    if response.status_code == 200:
-        response = response.text
-        json_var = json.loads(response)
-        json_str = json.dumps(json_var, indent=2)
-        print(json_str)
-    else:
-        print("Le GET sur '{}' a produit le code d'erreur {}.".format(
-            url_lister, response.status_code)
-        )
+    try:
+        reponse = requests.get(url_lister, params={'idul': idul})
+        if reponse.status_code == 200:
+            reponse = reponse.text
+            json_var = json.loads(reponse)
+            json_str = json.dumps(json_var, indent=2)
+            print(json_str)
+        else:
+            print("Le GET sur '{}' a produit le code d'erreur {}".format(url_lister, donnees.status_code))
+    except RuntimeError as erreur:
+        print(erreur)
+        
 def initialiser_partie(idul):
     url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
     response = requests.post(url_lister, data={'idul': idul})
