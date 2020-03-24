a4977ec921ff20a6bdddb309df362d303ecbc575
evcou16 <62434178+evcou16@users.noreply.github.com>
2020-03-24 16:24:57 -0400

    modification fonction afficher damier

--- quoridor.py
+++ quoridor.py
@@ -30,10 +30,13 @@ def afficher_damier_ascii(grille):
     }
     board_positions = 9
     espace_horizontal = ((board_positions * 4) - 1)
+    #permet davoir les tableaux dequivalences entre les adresses du jeu et notre tableau
     jeu_position_x = range(1, (board_positions * 4), 4)
     jeu_position_y = range(((board_positions - 1) * 2), -1, -2)
+    #permet la creation du tableau de jeu
     legende = "legende: "
     board = [legende]
+    # plateforme du jeu
     for i in reversed(range((board_positions * 2) -1)):
         if (i % 2) == 0:
             board += ligne(i)
@@ -44,12 +47,14 @@ def afficher_damier_ascii(grille):
             board += ["  |"]
             board += ([" "] * espace_horizontal)
             board += ["|\n"]
+    #ligne du bas du damier
     board += "--|" + ("-" * espace_horizontal) + "|\n"
     #numero ligne du bas
     board += (" " * 2) + "| "
     for i in range(1, board_positions):
         board += str(i) + (" " * 3)
     board += "{}\n".format(board_positions)
+    #insertion des joueurs dans le damier
     for num, joueur in enumerate(grille["joueurs"]):
         #ajout du joueur a la legende du damier
         legende += "{}={} ".format((num + 1), joueur["nom"])
@@ -60,13 +65,15 @@ def afficher_damier_ascii(grille):
                 (0 > position[1] > board_positions)):
             raise IndexError("Adresse du joueur invalide!")
         #calcul du decallage relatif au tableau
-        indice = (jeu_position_x[(position[0] - 1)] +
+    indice = (jeu_position_x[(position[0] - 1)] +
                     (jeu_position_y[(position[1] - 1)] * espace_horizontal))
-        decallage = ((((indice + 1) // espace_horizontal) * 2) + 2)
-        indice += decallage
-        board[indice] = str(num + 1)
+    decallage = ((((indice + 1) // espace_horizontal) * 2) + 2)
+    indice += decallage
+        #inserer le personnage dans le tableau du jeu
+    board[indice] = str(num + 1)
     #completion de la legende du tableau
     board[0] = legende + "\n" + (" " * 3) + ("-" * espace_horizontal) +"\n"
+    # insertion des murs horizontaux dans le damier
     for mur_h in grille["mur"]["horizontaux"]:
         #verif que la position est dans les contraintes
         if ((1 > mur_h[1] > (board_positions - 1)) or
@@ -77,8 +84,9 @@ def afficher_damier_ascii(grille):
         decallage = ((((indice + 1) // espace_horizontal) * 2) + 2)
         indice += decallage
         #iterer pour placer les 5 murs
-        for i in range(7):
+    for i in range(7):
             board[(indice + i)] = "-"
+    #insertion des murs verticaux
     for mur_v in grille["mur"]["verticaux"]:
         #verif que la position est dans les contraintes
         if (2 > mur_v[0] > board_positions) or (1 > mur_v[1] > board_positions):
@@ -92,6 +100,7 @@ def afficher_damier_ascii(grille):
             board[(indice - (i * (espace_horizontal + 2)))] = "|"
     #afficher le jeu sous forme dune chaine de caracteres
     print("".join(board))
+    return str(board)
     
   #if __name__=='__main__':
     #init = api.initialiser_partie('evcou16')
